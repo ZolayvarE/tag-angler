71831e065d47c5f2453462ffcf5bdde16fb3ebc9
ZolayvarE <ZolayvarECU@gmail.com>
2016-08-29 15:49:47 -0700

    store query results in a counting object

--- client/query/queryController.js
+++ client/query/queryController.js
@@ -1,6 +1,7 @@
 app.controller('queryController', ['$scope', '$location', '$http', function($scope, $location, $http) {
   $scope.searchTerm = '';
   $scope.list = [];
+  $scope.counter = {};
   $scope.add = function () {
     $scope.list.push($scope.searchTerm);
     $scope.searchTerm = '';
@@ -12,11 +13,10 @@ app.controller('queryController', ['$scope', '$location', '$http', function($sco
       url: '/query',
       data: JSON.stringify({ query: $scope.searchTerm })
     }).then(function (result) {
-      while ($scope.list.length) {
-        $scope.list.pop();
-      }
+      $scope.counter;
+      $scope.list = result.data;
       for (var i = 0; i < result.data.length; i++) {
-        $scope.list.push(result.data[i]);
+        $scope.counter[result.data[i]] = ++$scope.counter[result.data[i]] || 1;
       }
     }, function (err) {
       console.err(err);
