3676b4e94511ec2c5650c760a2da554de455dc40
ZolayvarE <ZolayvarECU@gmail.com>
2016-08-29 15:43:29 -0700

    Implement server querying from search form

--- client/query/queryController.js
+++ client/query/queryController.js
@@ -1,6 +1,27 @@
-app.controller('queryController', ['$scope', function ($scope) {
-  $scope.queryString = '';
-  $scope.query = function () {
-    
+app.controller('queryController', ['$scope', '$location', '$http', function($scope, $location, $http) {
+  $scope.searchTerm = '';
+  $scope.list = [];
+  $scope.add = function () {
+    $scope.list.push($scope.searchTerm);
+    $scope.searchTerm = '';
+  };
+  $scope.search = function () {
+    // console.log(JSON.stringify({ query: $scope.searchTerm }));
+    $http({
+      method: 'POST',
+      url: '/query',
+      data: JSON.stringify({ query: $scope.searchTerm })
+    }).then(function (result) {
+      while ($scope.list.length) {
+        $scope.list.pop();
+      }
+      for (var i = 0; i < result.data.length; i++) {
+        $scope.list.push(result.data[i]);
+      }
+    }, function (err) {
+      console.err(err);
+    });
+
+    $scope.searchTerm = '';
   };
 }]);
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/client'));
 
 var memCache = {};
 
-app.get('/query', function (req, res) {
+app.post('/query', function (req, res) {
   var searchTerm = req.body.query || 'aardvark';
   if (memCache[searchTerm]) {
     console.log('Serving tags for "' + searchTerm + '" from memCache');
@@ -25,15 +25,12 @@ app.get('/query', function (req, res) {
     }, function (error, results) {
       var videos = results.items;
       var videoIds = [];
-      
       var getVideoId = function (videoObject) { 
         return videoObject.id.videoId; 
       };
-
       for (var i = 0; i < videos.length; i++) {
         videoIds.push(getVideoId(videos[i]));
       }
-      
       console.log('Looking up tags for: "' + searchTerm + '"');
       Youtube.videos.list({
         part: 'snippet',
@@ -43,11 +40,9 @@ app.get('/query', function (req, res) {
       }, function (error, results, body) {
         var videos = results.items;
         var allTags = [];
-
         var getVideoTags = function (videoObject) {
           return videoObject.snippet.tags;
         };
-
         for (var i = 0; i < videos.length; i++) {
           var currentTags = getVideoTags(videos[i]);
           if (currentTags) {
