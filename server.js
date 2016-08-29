68834be6fd19f2ae211a7f0762fe2ac556738b99
ZolayvarE <ZolayvarECU@gmail.com>
2016-08-29 14:54:56 -0700

    Implement basic youtube api query

--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var Youtube = require('youtube-api');
+var keys = require('./secretKeys.js');
 var app = express();
 
 app.use(bodyParser());
 app.use(express.static(__dirname + '/client'));
 
-app.get('/', function (req, res) {
-  res.render('index');
+var memCache = {};
+
+app.get('/query', function (req, res) {
+  var searchTerm = req.body.query || 'aardvark';
+  if (memCache[searchTerm]) {
+    res.send(memCache[searchTerm]);
+  } else {
+    Youtube.search.list({
+      part: 'id',
+      maxResults: 10,
+      q: searchTerm,
+      type: 'video',
+      key: keys.youtube
+    }, function (error, results) {
+      var videos = results.items;
+      var videoIds = [];
+      
+      var getVideoId = function (videoObject) { 
+        return videoObject.id.videoId; 
+      };
+
+      for (var i = 0; i < videos.length; i++) {
+        videoIds.push(getVideoId(videos[i]));
+      }
+      
+      // Youtube.video.list({
+      //   part: 'snippet',
+      //   maxResults: 10,
+      //   id: videoIds,
+        
+      // }, function (error, results) {
+
+      // });
+
+      res.send(videoIds);
+    });
+  }
 });
 
 
