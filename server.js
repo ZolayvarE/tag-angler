09a7ae1604cc3615217512a5cdd9b43eccba5dd9
ZolayvarE <ZolayvarECU@gmail.com>
2016-08-29 11:54:09 -0700

    Create basic landing page, and npm start script

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser());
+app.use(express.static(__dirname + '/client'));
 
+app.get('/', function (req, res) {
+  res.render('index');
+});
 
 
 app.listen(3000);
