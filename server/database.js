a955ebf1fa16cd88ae5abea40bfa5ba6d1eeb6c0
ZolayvarE <ZolayvarECU@gmail.com>
2016-08-29 17:20:12 -0700

    Define new user schema for database

---
+++ server/database.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+  username: String,
+  password: String,
+  date: { type: Date, default: Date.now() }
+});
