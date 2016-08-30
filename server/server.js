88910471d0e56b6dc58d54349e8fe5e2e4106ce6
ZolayvarE <ZolayvarECU@gmail.com>
2016-08-29 20:09:22 -0700

    t
    render login page html using pug

--- server/server.js
+++ server/server.js
@@ -2,18 +2,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var partials = require('express-partials');
+var path = require('path');
+var pug = require('pug');
 var User = require('./database/userSchema.js');
 var query = require('./helpers/queryHelper.js');
 var keys = require('./config/secretKeys.js');
-
 var app = express();
 
-mongoose.connect('mongodb://localhost:27017', function () { console.log('connected to database!'); });
 app.use(bodyParser());
-app.use(express.static(__dirname + '/../client'));
+app.set('views', path.join(__dirname, '/../client'));
+app.set('view engine', 'pug');
 app.use(session({
   secret: keys.secret
 }));
+
+mongoose.connect('mongodb://localhost:27017', function () { console.log('connected to database!'); });
 
 app.post('/query', function (req, res) {
   if (!req.session.valid) {
@@ -25,19 +29,25 @@ app.post('/query', function (req, res) {
 });
 
 app.get('/login', function (req, res) {
-  res.send('oh boy. here we go again.');
+  res.render('login.pug');
 });
 
 app.get('/', function (req, res) {
   if (!req.session.valid) {
     res.redirect('/login');
   } else { 
-    res.render('index.html');
+    res.render('index.pug');
   }
 });
 
-app.get('/asdf', function (req, res) {
-  res.send('you dun goofed');
+app.use(express.static(path.join(__dirname, '/../client')));
+
+app.get('/*', function (req, res) {
+  if (req.session.valid) {
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
+  }
 });
 
 var port = process.env.port || 3571;
