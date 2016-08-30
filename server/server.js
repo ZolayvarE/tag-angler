12aa83dce510a588ffd9c3c5f5ca70ca31df0759
ZolayvarE <ZolayvarECU@gmail.com>
2016-08-29 19:22:17 -0700

    Attempt to serve up html for login page

--- server/server.js
+++ server/server.js
@@ -1,17 +1,44 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
+var session = require('express-session');
 var User = require('./database/userSchema.js');
-var query = require('./helpers/queryHelper.js')
+var query = require('./helpers/queryHelper.js');
+var keys = require('./config/secretKeys.js');
 
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017', function () { console.log('connected to database!'); });
 app.use(bodyParser());
 app.use(express.static(__dirname + '/../client'));
+app.use(session({
+  secret: keys.secret
+}));
 
-app.post('/query', query.grabTags);
+app.post('/query', function (req, res) {
+  if (!req.session.valid) {
+    console.log('redirecting!');
+    res.redirect('/login');
+  } else { 
+    query.grabTags(req, res);
+  }
+});
+
+app.get('/login', function (req, res) {
+  res.send('oh boy. here we go again.');
+});
+
+app.get('/', function (req, res) {
+  if (!req.session.valid) {
+    res.redirect('/login');
+  } else { 
+    res.render('index.html');
+  }
+});
+
+app.get('/asdf', function (req, res) {
+  res.send('you dun goofed');
+});
 
 var port = process.env.port || 3571;
 app.listen(port);
